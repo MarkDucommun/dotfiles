18323b185c1c13e23eedb63026ce04ce8ada13e0
Mark Ducommun <mark@giveforward.com>
2013-10-22 11:23:57 -0500

    dotfiles

---
+++ .bash/aliases.sh
@@ -0,0 +1,13 @@
+alias s="/usr/local/bin/subl"
+alias subl="/usr/local/bin/subl -n"
+
+alias gs="git status"
+alias ga="git add"
+alias gb="git branch"
+alias gc="git commit"
+alias go="git checkout"
+alias got="git"
+alias get="git"
+
+alias be="bundle exec"
+
